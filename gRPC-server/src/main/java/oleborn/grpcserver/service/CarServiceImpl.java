b1e127c05c9f8804cbf1c7727262983f0ae7773d
Denis <Oleborn@mail.ru>
2025-10-12 19:00:13 +0700

    добавлена логика обработки даты

--- gRPC-server/src/main/java/oleborn/grpcserver/service/CarServiceImpl.java
+++ gRPC-server/src/main/java/oleborn/grpcserver/service/CarServiceImpl.java
@@ -8,6 +8,11 @@ import oleborn.grpc.common.CarStatus;
 import oleborn.grpc.common.ListCarResponse;
 import org.springframework.grpc.server.service.GrpcService;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 // Аннотация, которая помечает класс как gRPC сервис для автоматического сканирования Spring'ом
 // Spring gRPC автоматически найдет этот класс и зарегистрирует его как gRPC endpoint
 @GrpcService
@@ -20,9 +25,12 @@ public class CarServiceImpl extends CarServiceGrpc.CarServiceImplBase {
             StreamObserver<ListCarResponse> responseObserver // Колбэк для отправки ответа клиенту (асинхронный поток)
     ) {
 
+        // Преобразуем Timestamp в читаемую дату
+        String formattedBirthDate = formatTimestamp(request.getUserBirthDate());
+
         log.info("Пользователю с именем {}, родившемуся: {}, на бюджет: {}, предложена машина.",
                 request.getUserName(),
-                request.getUserBirthDate(),
+                formattedBirthDate,
                 request.getBudget()
         );
 
@@ -44,5 +52,24 @@ public class CarServiceImpl extends CarServiceGrpc.CarServiceImplBase {
         // Завершение gRPC вызова - сигнализирует клиенту, что ответ завершен
         // Без этого вызова клиент будет ждать продолжения потока
         responseObserver.onCompleted();
+    }
+
+    private String formatTimestamp(com.google.protobuf.Timestamp timestamp) {
+        if (timestamp == null) {
+            return "не указана";
+        }
+
+        try {
+            Instant instant = Instant.ofEpochSecond(timestamp.getSeconds(), timestamp.getNanos());
+
+            LocalDateTime dateTime = LocalDateTime.ofInstant(instant, ZoneId.of("UTC"));
+            
+            return DateTimeFormatter.ofPattern("dd.MM.yyyy в HH:mm:ss").format(dateTime);
+
+
+        } catch (Exception e) {
+            log.warn("Ошибка при форматировании даты: {}", e.getMessage());
+            return "ошибка формата";
+        }
     }
 }
